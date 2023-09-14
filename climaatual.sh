3d43e7aadac35a98c50e6c72b39bbce6ddaa63fa
Gabriel Cerutti <42590905+Bill1300@users.noreply.github.com>
2023-09-14 12:52:33 -0300

    Add files via upload
    Tratado chamada de edição.
    Tratado link de repo.

--- climaatual.sh
+++ climaatual.sh
@@ -3,26 +3,28 @@
 function desinstalar() {
     rm -r ~/.ClimaAtual
     sudo rm /bin/climaatual
-    echo "Desinstalado com sucesso."
+    echo "🟧 Desinstalado com sucesso."
 }
 
 if [ -z "$1" ]; then
     python3 ~/.ClimaAtual/executar.py
 else
-    if [[ $1 == "-s" ]]; then
+    if [[ $1 == "-s" ]]; then #apresentarDadosSimples
         python3 ~/.ClimaAtual/executar.py -s
-    fi
-    if [[ $1 == "-d" ]]; then
-        echo "Você deseja confirmar a desinstalação? (S/N)"
+    elif [[ $1 == "-e" ]]; then #editarParametros
+        python3 ~/.ClimaAtual/executar.py -e
+    elif [[ $1 == "-d" ]]; then #desinstalar
+        echo "🟧 Você deseja confirmar a desinstalação? (S/N)" 
         read des
         if [[ $des == "s" || $des == "S" ]]; then
             desinstalar
         fi
         if [[ $des == "n" || $des == "N" ]]; then
-            echo "Operação cancelada."
+            echo "🟧 Operação cancelada."
         fi
-    fi
-    if [[ $1 == "-D" ]]; then
+    elif [[ $1 == "-D" ]]; then #desinstalarSemConfirmacao
         desinstalar
+    else #parametroDesconhecido
+        echo "🟧 Parâmetro desconhecido."
     fi
 fi
